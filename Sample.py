55e35216a4d67f61c6d202bf4f9c27aa8b4ad74b
Shweta Pandey <triptipandey77@gmail.com>
2025-09-10 15:22:55 +0530

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,90 @@
+#print('hello world')
+#print("hey how are you")
+
+
+'''
+print("Welcome")
+name = input("Enter your name:")
+print("Welcome", name)
+print("Welcome "  +name)
+print("$" *10)
+'''
+from operator import truediv
+
+'''
+#variables in python
+age = int(input("Enter your age:"))
+#whatever user types inside the age variable python considers it as string so we are typecasting it to integer
+print("You are", age*12, "months old")
+#converting age numbers in years into month and then printing it.
+
+#converting months into years of age
+age1 = int(input("Enter your age in months:"))
+print("You are", age1/12, "years old")
+'''
+#how to find type of a variable
+# a= 10
+# b= 20.02
+# c= "Hello I am a variable"
+# d= True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+#Arithmetic Operations
+# z= float(input("Enter value of z variable:"))
+# x= float(input("Enter value of x variable:"))
+# print(type(x))
+# print(type(z))
+# d= x-z
+# e= x*z
+# f= x/z
+# g= x%z
+# print(x)
+# print(z)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+#
+#comparison operators
+# j=50
+# k=100
+# print(k==j)
+# print(k!=j)
+# print(k>j)
+# print(k<j)
+# print(k<=j)
+# print(k>=j)
+
+#Logic operators
+# a= True
+# b= False
+# print(a and b)
+# print (a or b)
+# print (not a)
+
+#Assignment Operators
+# c = 10
+# d=20
+# c+=d
+# print(c)
+# c-=d
+# print(c)
+
+#membership operator
+# statement = 'I love my country'
+# target='love'
+# print(target in statement) #it will return true
+# print(target not in statement)
+
+#Identity Operators
+a=20
+b=10
+print(a is b)
+print(a is not b)
+
+exit()
+
